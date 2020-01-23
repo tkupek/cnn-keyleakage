369fae2a3b1d3fcfd0be479b1c5ad45eee5dea47
Tobias Kupek <mail@tkupek.eu>
2020-01-23 16:08:46 +0100

    prepare cifar10 key detection test

--- profile_models.py
+++ profile_models.py
@@ -9,11 +9,11 @@ TEST_SIZE = 10000
 
 if __name__ == "__main__":
 
-    (train_images, train_labels), (test_images, test_labels) = get_prepare_dataset.load_fashion_mnist(None)
+    (train_images, train_labels), (test_images, test_labels) = get_prepare_dataset.load_cifar10(None)
     test_images = test_images[:TEST_SIZE]
     test_labels = test_labels[:TEST_SIZE]
 
-    model = load_model(os.path.join('tmp', 'keyrecov0-0.h5'))
+    model = load_model(os.path.join('tmp', 'keyrecov1-0.h5'))
 
     profiled_layers = [layer.output for layer in model.layers if layer.name.startswith('activation')]
     profile = taboo_tools.profile_model(model, train_images, profiled_layers, 32)
